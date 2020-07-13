9aba3095f1c4f4cdeb46e056ca8f48c0a77b2bb3
mohiuddinbasha <babuirikimani@msitprogram.net>
2020-07-13 22:01:15 +0530

    Jul 13  22:01:14

--- 04-circlesintersect-Python/circlesintersect.py
+++ 04-circlesintersect-Python/circlesintersect.py
@@ -3,7 +3,11 @@
 # that describe the circle centered at (x1,y1) with radius r1, 
 # and the circle centered at (x2,y2) with radius r2, and returns True 
 # if the two circles intersect and False otherwise.
+import math
 
 def fun_circlesintersect(x1, y1, r1, x2, y2, r2):
 	# your code goes here
-	return False 
+	value = (x2-x1)**2+(y2-y1)**2
+	distance = math.sqrt(value)
+
+	return distance <= (r1+r2) 
