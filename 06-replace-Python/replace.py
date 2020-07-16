68bfd5de31b6639d5fbfaee89dcd9f0ce1f312dc
mohiuddinbasha <babuirikimani@msitprogram.net>
2020-07-16 07:19:49 +0530

    Jul 16  07:19:49

--- 06-replace-Python/replace.py
+++ 06-replace-Python/replace.py
@@ -6,8 +6,8 @@
 
 def fun_replace(s1, s2, s3):
 	for x in range(0,len(s1),1):
-		if s1[x:len(s2)] == s2:
-			s1 = s1[:x]+s2+s1[x+len(s2):]
+		if s1[x:len(s2)-1] == s2:
+			s1 = s1[:x]+s3+s1[x+len(s2):]
 	return s1
 
 print(fun_replace("helloworld123", "hello", "345"))
