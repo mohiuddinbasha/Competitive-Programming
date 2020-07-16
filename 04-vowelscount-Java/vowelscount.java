b24d328109896f0188af70d10fe690bc3728f2ca
mohiuddinbasha <babuirikimani@msitprogram.net>
2020-07-16 06:16:44 +0530

    Jul 16  06:16:44

--- 04-vowelscount-Java/vowelscount.java
+++ 04-vowelscount-Java/vowelscount.java
@@ -7,7 +7,17 @@
 class vowelscount {
 	public int fun_vowelscount(String s){
 		// your code goes here
-		return 0;
+		s = s.toLowerCase();
+		int count = 0;
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u') {
+				count++;
+			}
+		}
+		return count;
+	}
+	public static void main(String[] args) {
+		
 	}
 	
 }
