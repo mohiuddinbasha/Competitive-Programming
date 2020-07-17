df968556cdba225c7fdcb463e8508387d689884d
mohiuddinbasha <babuirikimani@msitprogram.net>
2020-07-17 18:15:43 +0530

    Jul 17  18:15:43

--- 11-findzerowithbisection-Python/findzerowithbisection.py
+++ 11-findzerowithbisection-Python/findzerowithbisection.py
@@ -22,4 +22,4 @@ def findzerowithbisection(x, epsilon):
 		else:
 			low = mid
 print(findzerowithbisection(25,0.01))
-print(findzerowithbisection(25,0.01)<0.01)
+print(findzerowithbisection(25,0.01)==0.01)
