524bb07eaf95081b2807e08b8e5570eba3fef820
mohiuddinbasha <babuirikimani@msitprogram.net>
2020-07-21 23:21:43 +0530

    Jul 21  23:21:43

--- 02-carrylessadd-Python/carrylessadd.py
+++ 02-carrylessadd-Python/carrylessadd.py
@@ -7,28 +7,51 @@
 
 def fun_carrylessadd(x, y):
 	s = 0
+	zero = False
+	count = 0
 	while x > 0 and y > 0:
 		first = x % 10
 		second = y % 10
 		v = first + second
 		if v > 9:
 			v = 10-v
+		if v == 0:
+			zero = True
+			count += 1
 		if s == 0:
 			s = v
 		else:
-			s *= 10
-			s += v
+			if zero:
+				s += v
+				s *= (10**count)
+				zero = False
+				count = 0
+			else:
+				s *= 10
+				s += v
 		x = x // 10
 		y = y // 10
 	if x != 0:
 		while x > 0:
-			s *= 10
-			s += x % 10
+			if zero:
+				s += x % 10
+				s *= (10**count)
+				zero = False
+				count = 0
+			else:
+				s *= 10
+				s += x % 10
 			x = x // 10
 	if y != 0:
 		while y > 0:
-			s *= 0
-			s += y % 10
+			if zero:
+				s += x % 10
+				s *= (10**count)
+				zero = False
+				count = 0
+			else:
+				s *= 10
+				s += x % 10
 			y = y // 10
 	s = str(s)
 	s = s[::-1]
