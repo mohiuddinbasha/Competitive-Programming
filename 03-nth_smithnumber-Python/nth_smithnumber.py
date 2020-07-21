e1c03e873b54c3a4c4dfc4a202e3c49b0d4bfdf4
mohiuddinbasha <babuirikimani@msitprogram.net>
2020-07-22 00:14:50 +0530

    Jul 22  00:14:50

--- 03-nth_smithnumber-Python/nth_smithnumber.py
+++ 03-nth_smithnumber-Python/nth_smithnumber.py
@@ -7,6 +7,37 @@
 # so fun_nthsmithnumber(0) should return 4
 # so fun_nthsmithnumber(1) should return 22
 
+def isPrime(n):
+    count = 0
+    for i in range(1,n+1):
+        if n % i == 0:
+            count += 1
+    return count == 2
 
 def fun_nth_smithnumber(n):
-    return 1
+    count = -1
+    temp = 2
+    while count < n:
+        if not isPrime(temp):
+            # print(temp)
+            s = []
+            for i in range(2,temp):
+                if isPrime(i) and temp % i == 0:
+                    q = temp // i
+                    s.append(str(q))
+            s = "".join(s)
+            t = list(s)
+            print(t)
+            s = 0
+            for x in t:
+                s += int(x)
+            temp_sum = 0
+            d = temp
+            while d > 0:
+                temp_sum += d % 10
+                d = d // 10
+            if s == temp_sum:
+                count += 1
+        temp += 1
+    return temp - 1
+print(fun_nth_smithnumber(0))
