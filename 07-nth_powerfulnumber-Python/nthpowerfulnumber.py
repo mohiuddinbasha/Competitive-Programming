74ee4e9db5a5fdfd7f2206c33c9306dc764922d0
mohiuddinbasha <babuirikimani@msitprogram.net>
2020-07-22 21:20:51 +0530

    Jul 22  21:20:51

--- 07-nth_powerfulnumber-Python/nthpowerfulnumber.py
+++ 07-nth_powerfulnumber-Python/nthpowerfulnumber.py
@@ -22,10 +22,11 @@ def nthpowerfulnumber(n):
 		for x in range(1, temp):
 			if temp % x == 0 and isPrime(x):
 				l.append(x)
-		if(all(temp % x**2 == 0 for x in l)):
+		print(l)
+		if(l != [] and all(temp % x**2 == 0 for x in l)):
 			print(temp)
 			count += 1
 		temp += 1
 	return temp - 1
-print(nthpowerfulnumber(10))
+print(nthpowerfulnumber(1))
 
