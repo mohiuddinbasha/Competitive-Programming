b95df30bcade0ffc36cbd632560c310956fdb9d2
mohiuddinbasha <babuirikimani@msitprogram.net>
2020-07-22 17:22:05 +0530

    Jul 22  17:22:05

--- 07-nth_powerfulnumber-Python/nthpowerfulnumber.py
+++ 07-nth_powerfulnumber-Python/nthpowerfulnumber.py
@@ -4,7 +4,26 @@
 # A number n is said to be Powerful Number if for every prime factor p of it, p2 also divides it. 
 # For example:- 36 is a powerful number. It is divisible by both 3 and square of 3 i.e, 9.
 
+def isPrime(n):
+	count = 0
+	for i in range(1,n+1):
+		if n % i == 0:
+			count += 1
+	return count == 2
 
 def nthpowerfulnumber(n):
 	# Your code goes here
-	pass
+	if n == 0:
+		return 1
+	count = 0
+	temp = 2
+	while count < n:
+		l = []
+		for x in range(1, temp):
+			if temp % x == 0 and isPrime(x):
+				l.append(x)
+		if(all(temp % x**2 == 0 for x in l)):
+			count += 1
+		temp += 1
+	return temp - 1
+
