06c135a41add2470f273adc3b52ee9bae584a048
mohiuddinbasha <babuirikimani@msitprogram.net>
2020-07-23 02:57:27 +0530

    Jul 23  02:57:27

--- 01-nth_tidynumber-Python/nth_tidynumber.py
+++ 01-nth_tidynumber-Python/nth_tidynumber.py
@@ -7,4 +7,17 @@
 # fun_nth_tidynumber(35) = 46
 
 def fun_nth_tidynumber(n):
-    return 0
+    count = -1
+    temp = 1
+    while count < n:
+        boolean = True
+        s = str(temp)
+        for i in range(len(s)-1):
+            if s[i] > s[i+1]:
+                boolean = False
+                break
+        if boolean:
+            count += 1
+        temp += 1
+    return temp - 1
+        
