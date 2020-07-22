0e5e46aa995552b46ffd40f928d55275eb219f47
mohiuddinbasha <babuirikimani@msitprogram.net>
2020-07-22 15:59:44 +0530

    Jul 22  15:59:44

--- 03-nth_lefttruncatableprime-Python/nth_lefttruncatableprime.py
+++ 03-nth_lefttruncatableprime-Python/nth_lefttruncatableprime.py
@@ -6,9 +6,29 @@
 # So nthLeftTruncatablePrime(0) retunearestKaprekarNumber(n)rns 2, and 
 # nthLeftTruncatablePrime(10) returns 53.
 
-
-
 import math
 
+def isPrime(n):
+    count = 0
+    for i in range(1,n+1):
+        if n % i == 0:
+            count += 1
+    return count == 2
+
 def fun_nth_lefttruncatableprime(n):
-    return 1
+    count = -1
+    temp = 2
+    while count < n:
+        if isPrime(temp):
+            boolean = True
+            t = temp
+            while t > 0:
+                if not isPrime(t % 10):
+                    boolean = False
+                t = t // 10
+            if boolean:
+                count += 1
+        temp += 1
+    return temp - 1
+
+print(fun_nth_lefttruncatableprime(1))
