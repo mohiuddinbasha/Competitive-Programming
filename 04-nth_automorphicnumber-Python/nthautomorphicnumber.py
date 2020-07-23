ad6b83a9b77ad7f1346d0ff2e534715b80699417
mohiuddinbasha <babuirikimani@msitprogram.net>
2020-07-23 06:26:47 +0530

    Jul 23  06:26:47

--- 04-nth_automorphicnumber-Python/nthautomorphicnumber.py
+++ 04-nth_automorphicnumber-Python/nthautomorphicnumber.py
@@ -14,34 +14,40 @@ def nthautomorphicnumbers(n):
 	# Your code goes here
 	if n == 1 or n == 2:
 		return n-1
-	count = 2
+	if n == 3:
+		return 5
+	if n == 4:
+		return 6
+	count = 4
 	temp1 = 5
 	temp2 = 6
+	value = -1
 	while count < n:
-		counter = 0
-		while True:
-			s1 = str(counter)+str(temp1)
-			if isAutomorphic(int(s1)):
+		while temp1 < temp2:
+			counter = 1
+			while True:
+				s1 = str(counter)+str(temp1)
+				if isAutomorphic(int(s1)):
+					break
+				counter += 1
+			temp1 = int(str(counter)+str(temp1))
+			count += 1
+			if count == n:
+				value = temp1
 				break
-			counter += 1
-		temp1 = int(str(counter)+str(temp1))
-		counter = 0
-		while True:
-			s2 = str(counter)+str(temp2)
-			if isAutomorphic(int(s2)):
+		if count == n:
+			break
+		while temp2 < temp1:
+			counter = 1
+			while True:
+				s2 = str(counter)+str(temp2)
+				if isAutomorphic(int(s2)):
+					break
+				counter += 1
+			temp2 = int(str(counter)+str(temp2))
+			count += 1
+			if count == n:
+				value = temp2
 				break
-			counter += 1
-		temp2 = int(str(counter)+str(temp2))
-		count += 2
-	small = big = -1
-	if temp1 < temp2:
-		small = temp1
-		big = temp2
-	else:
-		small = temp2
-		big = temp1
-	if n % 2 != 0:
-		return small
-	else:
-		return big
-print(nthautomorphicnumbers(5))
+	return value
+print(nthautomorphicnumbers(8))
