0c8524c1844a72e15a924646ff39110d14967a37
mohiuddinbasha <babuirikimani@msitprogram.net>
2020-07-25 13:51:03 +0530

    Jul 25  13:51:03

--- 08-getallpermutations-Python/test_getallpermutations.py
+++ 08-getallpermutations-Python/test_getallpermutations.py
@@ -7,11 +7,12 @@ from itertools import permutations
 @pytest.mark.parametrize('x',[
 	("abc"), ("abcd"), ("ab"), ("abcde"), 
 	("pqr"), ("pqrs"), ("pq"), ("pqrst"), 
-	("xyz"), ("xyza"), ("xy"), ("xyzab"),
+	# ("xyz"), ("xyza"), ("xy"), ("xyzab"),
 ])
 
 def test_getallpermutations(x):
 	l = list(permutations(x, r=len(x)))
-	l.sort(key = lambda x: x[0])
+	l.sort(key = lambda x: x[1])
 	print(l)
+	print(getallpermutations(x))
 	assert getallpermutations(x) == l
