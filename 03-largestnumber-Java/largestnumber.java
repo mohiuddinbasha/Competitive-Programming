671c5ff3fa29d152abb90bc7a7746661e27a8abd
mohiuddinbasha <babuirikimani@msitprogram.net>
2020-07-25 16:29:27 +0530

    Jul 25  16:29:27

---
+++ 03-largestnumber-Java/largestnumber.java
@@ -0,0 +1,16 @@
+// # largestNumber: Write the function largestNumber(text) that takes a string of text and 
+// # returns the largest int value that occurs within that text, or 0 if no such value occurs. 
+// # You may assume that the only numbers in the text are non-negative integers and 
+// # that numbers are always composed of consecutive digits (without commas, for example). For example:
+// #     	largestNumber("I saw 3 dogs, 17 cats, and 14 cows!")
+// # returns 17 (the int value 17, not the string "17"). And
+// #     	largestNumber("One person ate two hot dogs!")
+// # returns None (the value None, not the string "None").
+
+
+class largestnumber {
+	public int fun_largestnumber(String s){
+		return 0;	
+	}
+}
+	
