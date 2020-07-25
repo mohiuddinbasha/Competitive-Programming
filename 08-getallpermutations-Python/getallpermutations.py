2e77c77c6de34aa9a096a08a3bb55ef93c7f0f7c
mohiuddinbasha <babuirikimani@msitprogram.net>
2020-07-25 13:23:24 +0530

    Jul 25  13:23:24

--- 08-getallpermutations-Python/getallpermutations.py
+++ 08-getallpermutations-Python/getallpermutations.py
@@ -3,18 +3,30 @@
 # your program should print all 6 permutations e.g. [('a', 'b', 'c'), ('a', 'c', 'b'), ('b', 'a', 'c'), ('b', 'c', 'a'), ('c', 'a', 'b'), ('c', 'b', 'a')]
 
 l = []
-def recursion(string, p):
+def recursion(string, p, last):
 	if len(string)-1 == p:
+		l.append(string)
 		return
 	val = string[p]
 	for i in range(p,len(string)):
-		s = string[:i] + val + string[i+1:]
-		print(s)
-		# l.append(s)
-		# recursion(s,p+1)
+		if string[0] == last:
+			print(string)
+			string = list(string)
+			t = string[-1]
+			string[-1] = string[-2]
+			string[-2] = t
+			string = "".join(string)
+			print(string)
+		s = list(string)
+		temp = s[i]
+		s[i] = s[p]
+		s[p] = temp
+		s = "".join(s)
+		print("s: ",s)
+		recursion(s,p+1,last)
 
 def getallpermutations(x):
 	# Your code goes here
-	recursion(x,0)
+	recursion(x,0,x[-1])
 	return [tuple(x) for x in l]
 print(getallpermutations("abc"))
